2337cfe5fb7a36c8cdde60a9b1d13c1d819feefc
mayteolvera2909-dotcom <mayte.olvera2909@gmail.com>
2026-01-29 21:42:16 -0600

    Refactor contact form submission handler
    Updated contact form submission handler to improve clarity in comments.

--- js/contact.js
+++ js/contact.js
@@ -1,15 +1,16 @@
    document.getElementById("contactForm").addEventListener("submit", function(event) {
-        event.preventDefault(); // Prevents default form submission
+        event.preventDefault(); 
         
         // Show confirmation message
         const confirmationMessage = document.getElementById("confirmationMessage");
         confirmationMessage.style.display = "block";
         
-        // Clear input fields
+        // Clear  fields
         this.reset();
         
         // Set timeout to hide the message after 3 seconds
         setTimeout(() => {
             confirmationMessage.style.display = "none";
         }, 3000);
+
     });
