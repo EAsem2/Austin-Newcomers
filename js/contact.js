b0471809710000c1602571515afbcb6dd068ef54
EAsem2 <155112473+EAsem2@users.noreply.github.com>
2026-03-18 14:11:50 -0500

    Remove contact form submission handler
    Remove event listener for contact form submission.

--- js/contact.js
+++ js/contact.js
@@ -1,16 +1,1 @@
-   document.getElementById("contactForm").addEventListener("submit", function(event) {
-        event.preventDefault(); 
-        
-        // Show confirmation message
-        const confirmationMessage = document.getElementById("confirmationMessage");
-        confirmationMessage.style.display = "block";
-        
-        // Clear  fields
-        this.reset();
-        
-        // Set timeout to hide the message after 3 seconds
-        setTimeout(() => {
-            confirmationMessage.style.display = "none";
-        }, 3000);
 
-    });
